c949067b3b7d8f2ed771675053922d1cbb72ea11
cheerful-callback-1955 <jenniferetracy@gmail.com>
2020-11-11 19:01:05 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
 def generate_star_date
+  binding.pry
   (rand(100000) + 400000) / 10.0
 end
 
@@ -12,6 +13,4 @@ end
 def engage
   puts state_log(date)
   date = generate_star_date
- binding.pry
- puts 'nothing'
 end
